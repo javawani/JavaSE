b4aaa128be4b8be962d695e03128c04bfad64560
ITSC <ITSC@DESKTOP-ONE4R81>
2022-11-16 21:42:24 +0900

    자바 3일차 수업

--- src/day02/basic/MainClass07.java
+++ src/day02/basic/MainClass07.java
@@ -10,6 +10,7 @@ package day02.basic;
  * 
  * 기본형 - boolean, char, byte, short, int, long, float, double
  * 소문자로 시작한다  기본적으로 제공하는 변수 타입
+ * 참조형 - 기본형을 제외한 모든 클래스
  * 
  * 문자열은 char의 배열이다. 
  * 
@@ -20,7 +21,24 @@ public class MainClass07 {
 		// 변수 선언과 동시에 값 대입하기
 		String str = "안녕하세요.";
 		System.out.println("str : " + str);
-				
+		
+		/*
+		 * h e l l o
+		 * 0 1 2 3 4 = > 인덱스번호
+		 */
+		
+		String hello = "hello";
+	
+		
+		System.out.println("1번째 인덱스 :" + hello.charAt(1));
+		
+		
+		String addr = "서울 특별시 서대문구 신촌";
+		boolean isContain = addr str.contains("코리아아이티");
+		if(isContain) {
+			System.out.println("addr 에는 특별시 라는 문자열이 있어요!");
+			
+		}
 		
 	}
 
---
+++ src/day03/loop/MainClass04.java
@@ -0,0 +1,35 @@
+package day03.loop;
+
+/*
+ * for 반복문 연습
+ * 주말 숙제 2중 for문 코드 분석하기
+ * 
+ * 2번 숙제!
+ *      *
+ *     **
+ *    ***
+ *   ****
+ *  *****
+ *  
+ *  3번 숙제
+ *  가운데 정렬한 트리모양 만들어 오면된다.
+ *  
+ *  4번 숙제
+ *  다이아몬드 만들어오면 된다.
+ * 
+ * 
+ */
+
+
+public class MainClass04 {
+	public static void main(String[] args) {
+		// 1번 숙제 분석!
+		for(int i = 0; i < 7; i++) {
+			for(int j=0; j <= i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+	
+}
